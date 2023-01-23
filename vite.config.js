b047203f634236baa6df419642e8a602dc0184ec
baronin <baronin.yaroslav@gmail.com>
2023-01-23 09:25:45 +0200

    chore: change base in defineConfig

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'baronin.github.io',
+  base: '/',
   plugins: [
     vue(),
     VueI18nPlugin({ include: resolve(dirname(fileURLToPath(import.meta.url)), './locales'), }),
