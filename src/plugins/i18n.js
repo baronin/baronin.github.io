285d9fedd9c57a9e7462da19144f8fbcd0bef999
baronin <baronin.yaroslav@gmail.com>
2023-05-15 20:38:04 +0300

    chore: add meta description and change main language to ua

--- src/plugins/i18n.js
+++ src/plugins/i18n.js
@@ -6,8 +6,8 @@ import ua from '../locales/ua.json';
 const i18n = createI18n({
   legacy: false,
   globalInjection: true,
-  locale: "en",
-  fallbackLocale: "en",
+  locale: "ua",
+  fallbackLocale: "ua",
   availableLocales: ["en", "ua"],
   messages: { en, ua },
 });
