18cea4db6e19dff454eb01644be5414ec214ff63
baronin <baronin.yaroslav@gmail.com>
2023-05-27 13:45:09 +0300

    update build settings

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
+  publicPath: process.env.NODE_ENV === 'production' ? 'baronin.github.io' : '/',
   plugins: [
     vue(),
     VueI18nPlugin({ include: resolve(dirname(fileURLToPath(import.meta.url)), './locales'), }),
