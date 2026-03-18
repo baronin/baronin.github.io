c37dfb60b20b8a7170356e8dd9a0165f272921ae
baronin <baronin.yaroslav@gmail.com>
2026-03-18 18:49:44 +0200

    chore: remove unused files

--- src/api/index.js
+++
@@ -1,9 +0,0 @@
-import { createFakeUsers, createUser, getUsers, removeUserById, updateUser } from "@/api/users.js";
-
-export const api =  {
-  getUsers,
-  createFakeUsers,
-  removeUserById,
-  updateUser,
-  createUser
-}
